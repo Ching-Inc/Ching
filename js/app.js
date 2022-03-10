70422a1ee4cfc4db5966d0105eb6f318aa61d685
aosagwu <73572618+aosagwu@users.noreply.github.com>
2022-03-10 00:45:37 -0600

    Update

--- js/app.js
+++ js/app.js
@@ -49,19 +49,37 @@ document.getElementById("submit-btn").addEventListener("click", addToEmailListin
 function addToEmailListing() {
   // var script_url = "https://script.google.com/macros/s/AKfycbzVUFjypOFzFiPD7cZXnBdZ7hBx9MJ2lm_Bqm00ifofAWVikkzSDr9uWbzFjArbQOXqiw/exec";
   var script_url = "https://script.google.com/macros/s/AKfycbz8Spu5n_KcLIh0IkNiQoH4k83PXpfD2QQ3ge74-U20/dev";
-  $.getJSON(
-    script_url + "?callback=?",
-    {
-      method:"doGet",
-      "fname": document.getElementById("fname").value,
-      "lname": document.getElementById("lname").value,
-      "phone": document.getElementById("phone").value,
-      "email": document.getElementById("email").value
-      // success: function (response) { 
-      //   // alert(JSON.stringify(data)); 
-      //   // console.log(JSON.stringify(data)));
-      //   console.log(response);
-      // }
-    }
-  );
+  var contact = document.getElementById("contact").value;
+  var phone  = ("" + contact).replace(/\D/g, "");  
+  var match  = contact.match(/\S+@\S+\.\S+/);
+  var match1 = contact.match(/^(\d{3})(\d{3})(\d{4})$/);
+  var type   = "invalid";
+
+  if(match)
+    type = "Email";
+  else if(match1) {
+    type    = "Phone";
+    contact = '(' + match1[1] + ') ' + match1[2] + '-' + match1[3];
+  }
+
+  if(contact != "" && type != "invalid") {
+    $.getJSON(
+      script_url + "?callback=?",
+      {
+        method:"doGet",
+        "contact": contact,
+        "type": type
+        // success: function (response) { 
+        //   // alert(JSON.stringify(data)); 
+        //   // console.log(JSON.stringify(data)));
+        //   console.log(response);
+        // }
+      }
+    );
+
+    document.getElementById("join-form").hidden = true;
+    document.getElementById("join-success").hidden = false;
+  }
+  else 
+    document.getElementById("contact").style.borderColor = "red";
 }
