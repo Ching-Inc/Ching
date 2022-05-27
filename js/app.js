7a9919bcfcbbbf8356acae27140be967828d759e
aosagwu <73572618+aosagwu@users.noreply.github.com>
2022-05-26 23:56:51 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -18,13 +18,8 @@ $(document).ready(function(){
 });
 
 function addToEmailListing() {
-<<<<<<< Updated upstream
   // var script_url = "https://script.google.com/macros/s/AKfycbzVUFjypOFzFiPD7cZXnBdZ7hBx9MJ2lm_Bqm00ifofAWVikkzSDr9uWbzFjArbQOXqiw/exec";
-  var script_url = "https://script.google.com/macros/s/AKfycbwvWj0eARaAp8EkoWKp_9SmgyXcZuGQYyoS48N_kBk/dev";
-=======
-  // var script_url = "https://script.google.com/macros/s/AKfycbzHgC1GV1qVM2RcfNADSC9E6w5suEqxYB7R1BbCUT1wp1mKf0RlitESvp1kd6YngQac/exec";
-  var script_url = "https://script.google.com/macros/s/AKfycbwcQwAxpJs6K0CDLpAuYICS8ZAvUzvMhmfLf-QdfUQ/dev";
->>>>>>> Stashed changes
+  var script_url = "https://script.google.com/macros/s/AKfycbzik4rRatWLuj364oP0cZYtz2NlB18aZLej3MU-JDA/dev";
   var contact = document.getElementById("contact").value;
   var match  = contact.match(/\S+@\S+\.\S+/);
   var match1 = contact.match(/^(\d{3})(\d{3})(\d{4})$/);
@@ -44,7 +39,6 @@ function addToEmailListing() {
     $.getJSON(
       script_url + "?callback=?",
       {
-<<<<<<< Updated upstream
         method    :"doGet",
         "contact" : contact,
         "type"    : type
@@ -52,14 +46,5 @@ function addToEmailListing() {
     );
     document.getElementById("join-form").hidden = true;
     // document.getElementById("join-success").hidden = false;
-=======
-        method:"doGet",
-        "contact": contact,
-        "type": type
-      }
-    );
-    document.getElementById("join-form").hidden = true;
-    // document.getElementById("join-success").hidden = false;s
->>>>>>> Stashed changes
   }
 }
